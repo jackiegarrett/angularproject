a42cdb94b39390bee60bc32075e41f08a287d771
austin <austinkandt@gmail.com>
2020-10-19 20:51:50 -0400

    Added basic routing array

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,9 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+import { WatchlistPageComponent } from '../watchlist-page/watchlist-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movie-list',
+    component: MovieListComponent
+  },
+  {
+    path: 'watch-list',
+    component: WatchlistPageComponent
+  },
+  // {
+  //   path: 'movie-info/:id',
+  //   // component: ''
+  // },
+  {
+    path: '',
+    redirectTo: '/movie-list',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: MovieListComponent
+  },
+];
 
 @NgModule({
   imports: [
